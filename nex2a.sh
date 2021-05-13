d494aab2ece57e569df60ef072be361158ee5f56
Patrick <hawkpatrick@gmail.com>
2021-05-14 01:48:05 +0200

    Some more

--- nex2a.sh
+++ nex2a.sh
@@ -3,9 +3,20 @@
 set -e
 
 source secrets.sh
+source env.properties
+
+data_directory=$NEXTCLOUD_DATA_DIRECTORY
+mkdir "$data_directory" || echo "$data_directory was not created. Probably already exists"
+
+# Für den Ordner "$data_directory" wird der Owner wie folgt gesetzt:
+# User "www-data" und Gruppe "pho".
+# Dadurch kann der User www-data Vollzugreifen (für nextcloud nötig) und die Gruppe pho ebenfalls. 
+# Letzteres ist nötig, damit man über den Datei-Explorer zugreifen kann, wenn man möchte.
+sudo groupadd -f pho
+sudo chown -R www-data:pho "$data_directory"
 
 # Start des Nextcloud-Containers
-#  * "data" muss nach /var/www/html/data gemountet werden. 
+#  * "$data_directory" muss nach /var/www/html/data gemountet werden. 
 #  * OC_PASS muss gesetzt werden, damit ein User-Passwort per Umgebungsvariable gesetzt werden kann
 #  * skeleton wird gemountet, um Standard-Dateien zu überschreiben (wir wollen gar keine)
 
@@ -15,7 +26,7 @@ sudo docker run --name nextcloud -d \
 -e NEXTCLOUD_ADMIN_PASSWORD=$ADMIN_PASSWORD \
 -e OC_PASS=$PATRICK_PASSWORD \
 -p 8080:80 \
--v "$PWD/data":/var/www/html/data \
+-v "$data_directory":/var/www/html/data \
 -v "$PWD/skeleton":/tmp/myskeleton \
 nextcloud
 
@@ -24,8 +35,10 @@ echo "Docker container nextcloud created. Now waiting for it to come up. Therfor
 startup_status_code="0"
 while [[ "200" != $startup_status_code ]]; do
   sleep 5
-  startup_status_code=$(curl --write-out %{http_code} --silent --output /dev/null localhost:8080/login) || echo "curl failed. Waiting for 5 seconds and try again."
+  startup_status_code=$(curl --max-time 2 --write-out %{http_code} --silent --output /dev/null localhost:8080/login) || echo "curl failed. Waiting for 5 seconds and try again."
 done
+
+echo "It seems that container is up and running. Now configurations"
 
 # Setze das skeletondirectory (enthält Standard-Dateien für neue User) auf das gemountete leere Verzeichnis
 sudo docker exec --user www-data nextcloud php occ config:system:set skeletondirectory --value='/tmp/myskeleton'
@@ -38,21 +51,19 @@ sudo docker exec --user www-data nextcloud php occ config:system:set trusted_dom
 
 sudo docker exec --user www-data nextcloud php occ user:add --display-name="Patrick" --group="users" --password-from-env patrick
 
-
 # Die Daten sollen ins nextcloud-Daten-Verzeichnis für User patrick kopiert werden, 
-# also unter ./data/patrick/files. Dies ist bei nextcloud vorgegeben. 
-sudo mkdir -p ./data/patrick/files
+# also unter $data_directory/patrick/files. Dies ist bei nextcloud vorgegeben. 
+sudo mkdir -p $data_directory/patrick/files
 
 # Annahme: Die zu kopierenden Daten liegen unter /home/$$USRR/Pictures
-sudo cp -r "/home/$USER/Pictures" "data/patrick/files/Pictures"
-sudo cp -r "/home/$USER/Videos" "data/patrick/files/Videos"
+sudo cp -r "/home/$USER/Pictures" "$data_directory/patrick/files/Pictures"
+sudo cp -r "/home/$USER/Videos" "$data_directory/patrick/files/Videos"
 
 
-# Für den Ordner "data" wird der Owner wie folgt gesetzt: 
+# Für den Ordner "$data_directory" wird der Owner wie folgt gesetzt: 
 # User "www-data" und Gruppe "pho". 
-# Dadurch kann der User www-data Vollzugreifen (für nextcloud nötig) und die Gruppe pho ebenfalls. # Letzteres ist nötig, damit man über den Datei-Explorer zugreifen kann, wenn man möchte. 
-sudo groupadd -f pho
-sudo chown -R www-data:pho data
+# (Wiederholung von oben)
+sudo chown -R www-data:pho "$data_directory"
 
 
 # Mit occ einen File-Scan durchführen
