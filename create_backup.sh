61fdb5fc930bd5f2807567dbf292bb70a812b8f6
Patrick <hawkpatrick@gmail.com>
2021-05-15 21:41:56 +0200

    minor fixes

--- create_backup.sh
+++ create_backup.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 
 set -e
-
+cd /home/pi/workspace/nextcloud
 source env.properties
+
 backups_root_dir=$BACKUPS_ROOT_DIRECOTRY
 nextcloud_data_dir=$NEXTCLOUD_DATA_DIRECTORY
 
-mkdir -p $backups_root_dir
+if [ -d "$backups_root_dir" ]; then
+  echo "Root directory for backups is $backups_root_dir"
+else 
+  echo "Directory for backups should be $backups_root_dir but it does not exist. Aborting"
+  exit 1
+fi
+
 backup_foldername="$backups_root_dir/"$(date +'%Y-%m-%d')
-rm -rf $backup_foldername
-sudo rsync -Aavx "$nextcloud_data_dir/" "$backup_foldername/"
-sudo rsync -avh "$nextcloud_data_dir/" "$backups_root_dir/latest/" --delete
+sudo rsync -Aavx "$nextcloud_data_dir/patrick/files" "$backup_foldername/"
+sudo rsync -avh "$nextcloud_data_dir/patrick/files" "$backups_root_dir/latest/" --delete
 
 ./send_mail.sh 'success'
--- initial_import_pictures.sh
+++ initial_import_pictures.sh
@@ -3,3 +3,5 @@
 set -e
 
 sudo rsync -avh /media/pi/MyBook/media/pictures/ /media/pi/MyBook/nextcloud_data/patrick/files/Pictures/
+
+sudo /home/pi/workspace/nextcloud/occ_file_scan.sh
